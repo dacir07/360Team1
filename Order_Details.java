02cd217541afb4aac6ee6b1f85d5cf47e08de30c
atnguy43 <99783158+atnguy43@users.noreply.github.com>
2022-03-26 15:17:09 -0700

    Update Order_Details.java

--- Order_Details.java
+++ Order_Details.java
@@ -1,3 +1,12 @@
 public class Order_Details() {
+  public int orderID;
+  public int trackingID;
+  public string orderStatus;
   
+  public int calculatePendingOrders() {
+    
+  }
+  public bool cancelOrder() {
+    
+  }
 }
